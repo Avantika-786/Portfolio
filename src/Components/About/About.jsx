0f7895c412bf25e16f59e6806266b39cbc837df1
Avantika-786 <avantikaharale81@gmail.com>
2024-04-26 00:53:27 +0530

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+function About() {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me Sitting with laptop"
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>PUNE INSTITUTE OF COMPUTER TECHNOLOGY</h3>
+              <p>B.E Electronics and Telecommunication</p>
+              <p>
+                2021-2025 <br />
+                CGPA:9.0
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor" />
+            <div className={styles.aboutItemText}>
+              <h3>S.M.LOHIA JUNIOR COLLEGE</h3>
+              <p>
+                PCMB-MAHARASHTRA STATE BOARD
+                <br />
+                2021
+                <br />
+                MHTCET Percentile‑94.47
+                <br /> 12th Percentage‑92.50
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor" />
+            <div className={styles.aboutItemText}>
+              <h3>SHRI.JYOTIRLING VIDYAMANDIR KOLHAPUR</h3>
+              <p>
+                2019 <br />
+                10th Percentage‑90.40
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:avantikaharale81@email.com">
+            avantikaharale81email@email.com
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/avantika-harale-537105236">
+            linkedin.com/Avantika
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Avantika-786">github.com/Avantika</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+export default Contact;
